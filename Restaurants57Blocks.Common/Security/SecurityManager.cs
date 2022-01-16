4f3c567b44734e356c42cfb7cea0027643bb19ee
yeinerrestrepo01 <yeinermeri@gmail.com>
2022-01-16 14:54:38 -0500

    creacion de SecurityManager

---
+++ Restaurants57Blocks.Common/Security/SecurityManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Restaurants57Blocks.Common.Security
+{
+    public  class SecurityManager
+    {
+        static readonly string password = "P455W0rd";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <returns></returns>
+        public static string EncryptSHA512(string plainText)
+        {
+            if (plainText == null)
+            {
+                return null;
+            }
+            // Get the bytes of the string
+            var bytesToBeEncrypted = Encoding.UTF8.GetBytes(plainText);
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            // Hash the password with SHA256
+            passwordBytes = SHA512.Create().ComputeHash(passwordBytes);
+
+            var bytesEncrypted = Encrypt(bytesToBeEncrypted, passwordBytes);
+
+            return Convert.ToBase64String(bytesEncrypted);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bytesToBeEncrypted"></param>
+        /// <param name="passwordBytes"></param>
+        /// <returns></returns>
+
+        private static byte[] Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+            var saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+    }
+}
