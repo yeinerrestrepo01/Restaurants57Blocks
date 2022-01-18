4c5935a5cff8fcda4bdbff838269163a49093b0e
yeinerrestrepo01 <yeinermeri@gmail.com>
2022-01-18 07:23:41 -0500

    Alter_Table_User, se eliminan los campos de email phone

--- Restaurants57Blocks.Domain/Entities/User.cs
+++ Restaurants57Blocks.Domain/Entities/User.cs
@@ -11,9 +11,7 @@ namespace Restaurants57Blocks.Domain.Entities
     {
         [Key]
         public int Id { get; set; }
-        public string FullName { get; set; }
         public string Email { get; set; }
-        public string Phone { get; set; }
         public string Password { get; set; }
         public DateTime DateRegister { get; set; }
         public bool Status { get; set; } = true;
---
+++ Restaurants57Blocks.Infrastructure/Migrations/20220118122210_Alter_Table_User.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Restaurants57Blocks.Infrastructure.Migrations
+{
+    public partial class Alter_Table_User : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "User");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
